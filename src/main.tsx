12047f080740b59a1c0b186f6a6aab1610704512
Vicente Jorquera <jorquerav07@gmail.com>
2026-05-14 22:51:33 -0400

    se mejoran estilos

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./ui/App";
+import "./theme.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
